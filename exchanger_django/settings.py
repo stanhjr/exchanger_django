cf33a1947d836d3b4f267377e10871e541c34740
stanhjr <stanislav.o@cybearsoft.com>
2022-11-20 13:23:20 +0200

    add media prefix

--- exchanger_django/settings.py
+++ exchanger_django/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/home/static')
-MEDIA_URL = 'media/'
+MEDIA_URL = 'api/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
