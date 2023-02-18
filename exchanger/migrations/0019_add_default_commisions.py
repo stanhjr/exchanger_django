e1fa50f27dd8392c8b6332a949ad112621f507d2
Stan <stan@MacBook-Air-Stan.local>
2023-02-18 16:11:18 +0200

    fix migrations

--- exchanger/migrations/0019_add_default_commisions.py
+++ exchanger/migrations/0019_add_default_commisions.py
@@ -6,7 +6,6 @@ from exchanger.models import Commissions
 def create_default_commissions(*args, **kwargs):
     Commissions.objects.create(
         white_bit_commission=0.01,
-        service_commission=0.05
     )
 
 
